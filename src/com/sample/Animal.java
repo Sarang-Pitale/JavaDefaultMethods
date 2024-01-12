ede7b0888780b384ba6aaa9304e429e85fa43a80
Sarang <sarangp@celcomsolutions.com>
2024-01-12 15:13:00 +0530

    Initial Commit

---
+++ src/com/sample/Animal.java
@@ -0,0 +1,12 @@
+package com.sample;
+
+ public interface Animal {  // Interface
+    int noOfLegs=4;
+    int age=1;
+    int horns=2;
+    int tail=1;
+    void sleep(); //abstract method
+    void eat();  //abstract method
+    default void brethe(){ //default Method
+        System.out.println("hmmmm");}
+}
---
+++ src/com/sample/DemoInterface.java
@@ -0,0 +1,8 @@
+package com.sample;
+
+public class DemoInterface {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.brethe();
+    }
+}
